c42b99922e19261b1e8156aeb929afda6f107a7e
Duke <dukethanakorn@gmail.com>
2025-01-08 20:44:12 +0700

    Before flatList

---
+++ src/features/restaurants/components/restaurant-info-card.styles.js
@@ -0,0 +1,47 @@
+import styled from "styled-components/native";
+import { Card } from "react-native-paper";
+
+
+// Styled components
+export const StyledCard = styled(Card)`
+  margin: ${(props) => props.theme.space[3]}; /* Add spacing around the card */
+  elevation: 4; /* Optional: Add shadow for Android */
+  border-radius: ${(props) =>
+    props.theme.space[2]}; /* Optional: Rounded corners */
+  background-color: ${(props) =>
+    props.theme.colors.bg.primary}; /* Ensure consistent background color */
+`;
+
+export const StyledCardCover = styled(Card.Cover)`
+  height: ${(props) => props.theme.sizes[5]}; /* Adjust height of the image */
+`;
+
+export const Title = styled.Text`
+  font-family: ${(props) => props.theme.fonts.monospace};
+  font-size: 20px;
+  font-weight: ${(props) => props.theme.fontWeights.bold};
+  text-align: center; /* Center align the text */
+`;
+
+export const Info = styled.View`
+  margin-top: ${(props) =>
+    props.theme.space[2]}; /* Add spacing between image and text */
+`;
+
+export const RatingRow = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between; /* Space out elements horizontally */
+  padding-top: ${(props) => props.theme.space[1]};
+  padding-bottom: ${(props) => props.theme.space[1]};
+`;
+
+export const Stars = styled.View`
+  flex-direction: row; /* Align stars in a row */
+`;
+
+export const Address = styled.Text`
+  margin-top: ${(props) => props.theme.space[2]};
+  font-family: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSizes.body};
+`;
--- src/features/restaurants/components/restaurant-info.component.js
+++ src/features/restaurants/components/restaurant-info.component.js
@@ -1,54 +1,19 @@
 import React from "react";
 import styled from "styled-components/native";
 import { Card } from "react-native-paper";
-import { SvgXml } from 'react-native-svg';
+import { SvgXml } from "react-native-svg";
 import { Text, Image, View } from "react-native";
 import star from "../../../../assets/star";
 import open from "../../../../assets/open";
-
-
-// Styled components
-const StyledCard = styled(Card)`
-  margin: ${(props) => props.theme.space[3]}; /* Add spacing around the card */
-  elevation: 4; /* Optional: Add shadow for Android */
-  border-radius: ${(props) => props.theme.space[2]}; /* Optional: Rounded corners */
-  background-color: ${(props) => props.theme.colors.bg.primary}; /* Ensure consistent background color */
-`;
-
-const StyledCardCover = styled(Card.Cover)`
-  height: ${(props) => props.theme.sizes[5]}; /* Adjust height of the image */
-`;
-
-
-const Title = styled.Text`
-  font-family: ${(props) => props.theme.fonts.monospace};
-  font-size: 20px;
-  font-weight: ${(props) => props.theme.fontWeights.bold};
-  text-align: center; /* Center align the text */
-`;
-
-const Info = styled.View`
-  margin-top: ${(props) => props.theme.space[2]}; /* Add spacing between image and text */
-`;
-
-const RatingRow = styled.View`
-  flex-direction: row; 
-  align-items: center; 
-  justify-content: space-between; /* Space out elements horizontally */
-  padding-top: ${(props) => props.theme.space[1]};
-  padding-bottom: ${(props) => props.theme.space[1]};
-`;
-
-const Stars = styled.View`
-  flex-direction: row; /* Align stars in a row */
-`;
-
-
-const Address = styled.Text`
-  margin-top: ${(props) => props.theme.space[2]};
-  font-family: ${(props) => props.theme.fonts.body};
-  font-size:  ${(props) => props.theme.fontSizes.body};
-`;
+import {
+  StyledCard,
+  StyledCardCover,
+  Title,
+  Info,
+  RatingRow,
+  Stars,
+  Address,
+} from "./restaurant-info-card.styles";
 
 // Component
 export const RestaurantInfo = ({ restaurant = {} }) => {
@@ -64,7 +29,7 @@ export const RestaurantInfo = ({ restaurant = {} }) => {
     isClosedTemporarily = true,
   } = restaurant;
 
-const ratingArray = Array.from(new Array(Math.floor(rating)));
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
 
   return (
     <StyledCard>
@@ -74,12 +39,12 @@ const ratingArray = Array.from(new Array(Math.floor(rating)));
       {/* Display Restaurant Name */}
       <Card.Content>
         <Info>
-        <Title>{name}</Title>
+          <Title>{name}</Title>
 
           {/* Rating Row */}
           <RatingRow>
-      {/* Stars */}
-      <Stars>
+            {/* Stars */}
+            <Stars>
               {ratingArray.map((_, index) => (
                 <SvgXml key={index} xml={star} width={20} height={20} />
               ))}
@@ -94,22 +59,16 @@ const ratingArray = Array.from(new Array(Math.floor(rating)));
                 </Text>
               )}
 
-      {/* Open Icon */}
-      {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
-
+              {/* Open Icon */}
+              {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
 
               {/* Padding and Icon */}
               <View style={{ paddingLeft: 16 }} />
-              <Image
-                style={{ width: 15, height: 15 }}
-                source={{ uri: icon }}
-              />
-   </View>
-      </RatingRow>
+              <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
+            </View>
+          </RatingRow>
 
-
-
-        <Address>{address}</Address>
+          <Address>{address}</Address>
         </Info>
       </Card.Content>
     </StyledCard>
