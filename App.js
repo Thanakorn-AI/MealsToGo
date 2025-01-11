16f7320b78b49b9659fc93abd2c143ffd02413c2
Thanakorn Kitviriya <dukethanakorn@gmail.comc>
2025-01-11 21:26:41 +0700

    Before Setting Up Restaurant Detail Navigation

--- App.js
+++ App.js
@@ -1,7 +1,5 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
-import { Text, View } from "react-native";
-import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 import { ThemeProvider } from "styled-components/native";
 import { theme } from "./src/infrastructure/theme";
 import {
@@ -13,61 +11,12 @@ import {
   Lato_400Regular,
   Lato_700Bold,
 } from "@expo-google-fonts/lato";
-import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { Ionicons } from "@expo/vector-icons";
-import { SafeContainer } from "./src/components/utility/safe-container.component";
-import { restaurantsRequest } from "./src/services/restaurants/restaurants.service";
 import { RestaurantsContextProvider } from "./src/services/restaurants/restaurants.context";
 import { LocationContextProvider } from "./src/services/location/location.context";
-
+import { Navigation } from "./src/infrastructure/navigation";
 
 // const isAndroid = Platform.OS === "android";
 // console.log(StatusBar.currentHeight);
-function MapScreen() {
-  return (
-    <SafeContainer>
-      <Text>Map</Text>
-    </SafeContainer>
-  );
-}
-
-function SettingsScreen() {
-  return (
-    <SafeContainer>
-      <Text>Settings!</Text>
-    </SafeContainer>
-  );
-}
-
-const Tab = createBottomTabNavigator();
-
-function MyTabs() {
-  return (
-    <Tab.Navigator
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ color, size }) => {
-          let iconName;
-
-          if (route.name === "Restaurants") {
-            iconName = "restaurant";
-          } else if (route.name === "Map") {
-            iconName = "map";
-          } else if (route.name === "Settings") {
-            iconName = "settings";
-          }
-
-          // Return the icon component
-          return <Ionicons name={iconName} size={size} color={color} />;
-        },
-      })}
-    >
-      <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
-      <Tab.Screen name="Map" component={MapScreen} />
-      <Tab.Screen name="Settings" component={SettingsScreen} />
-    </Tab.Navigator>
-  );
-}
 
 export default function Index() {
   const [oswaldLoaded] = useOswald({
@@ -85,12 +34,10 @@ export default function Index() {
   return (
     <>
       <ThemeProvider theme={theme}>
-      <LocationContextProvider>
-        <RestaurantsContextProvider>
-          <NavigationContainer>
-            <MyTabs />
-          </NavigationContainer>
-        </RestaurantsContextProvider>
+        <LocationContextProvider>
+          <RestaurantsContextProvider>
+            <Navigation />
+          </RestaurantsContextProvider>
         </LocationContextProvider>
       </ThemeProvider>
       <ExpoStatusBar style="auto" />
---
+++ src/infrastructure/navigation/app.navigator.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
+import { Text } from "react-native";
+
+import { SafeContainer } from "../../components/utility/safe-container.component";
+import { RestaurantsNavigator } from "./restaurants.navigator";
+
+const Tab = createBottomTabNavigator();
+
+function MapScreen() {
+  return (
+    <SafeContainer>
+      <Text>Map</Text>
+    </SafeContainer>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <SafeContainer>
+      <Text>Settings!</Text>
+    </SafeContainer>
+  );
+}
+
+export const AppNavigator = () => (
+  <NavigationContainer>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          let iconName;
+
+          if (route.name === "Restaurants") {
+            iconName = "restaurant";
+          } else if (route.name === "Map") {
+            iconName = "map";
+          } else if (route.name === "Settings") {
+            iconName = "settings";
+          }
+
+          // Return the icon component
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
+    >
+      <Tab.Screen name="Restaurants" component={RestaurantsNavigator} />
+      <Tab.Screen name="Map" component={MapScreen} />
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+    </Tab.Navigator>
+  </NavigationContainer>
+);
